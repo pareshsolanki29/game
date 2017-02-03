08e4fd78d73b39c5d394a6b82aae02d9cd992805
paresh solanki <bandralites@gmail.com>
2017-02-03 00:20:04 -0500

    new function

--- gameStuff/reverseSentence.cs
+++ gameStuff/reverseSentence.cs
@@ -13,4 +13,19 @@
 
 
 
-            
+for (int x = 2; x < 100; x++)
+    {
+        int isPrime = 0;
+        for (int y = 1; y < x; y++)
+        {
+            if (x % y == 0)
+                isPrime++;
+
+            if(isPrime == 2) break;
+        }
+        if(isPrime != 2)
+           Console.WriteLine(x);
+
+        isPrime = 0;
+    }
+    Console.ReadKey();
